611930161657a95b4139180d5553d8827e179e46
Clozer <alvarodgonzalo@gmail.com>
2024-04-20 20:36:10 +0200

    🎨 Format files for 2 tabs

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App: React.FC = () => {
-    return <div className='text-3xl font-bold'>Hello, World :)</div>;
+  return <div className='text-3xl font-bold'>Hello, World :)</div>;
 };
 
 export default App;
